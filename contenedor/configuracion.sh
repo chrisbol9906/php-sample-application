4cf3c8827bd45915179f6e2d9fea3b9091252780
chrisbol9906 <89878988+chrisbol9906@users.noreply.github.com>
2021-09-02 22:07:19 -0500

    Add files via upload

---
+++ contenedor/configuracion.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+#conf db
+
+docker exec -i db_node mysql -u root --password=samplepass -e "ALTER USER sampleuser IDENTIFIED WITH mysql_native_password BY 'samplepass';"
+
+docker exec -i db_node sh -c 'exec mysql -u root --password=samplepass sample' < Db_node/db.sql
