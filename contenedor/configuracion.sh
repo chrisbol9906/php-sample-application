f0f145b760638640350e6227eb7c0b613d3f9af6
chrisbol9906 <89878988+chrisbol9906@users.noreply.github.com>
2021-09-02 22:12:42 -0500

    Update configuracion.sh

--- contenedor/configuracion.sh
+++ contenedor/configuracion.sh
@@ -4,4 +4,4 @@
 
 docker exec -i db_node mysql -u root --password=samplepass -e "ALTER USER sampleuser IDENTIFIED WITH mysql_native_password BY 'samplepass';"
 
-docker exec -i db_node sh -c 'exec mysql -u root --password=samplepass sample' < Db_node/db.sql
+docker exec -i db_node sh -c 'exec mysql -u root --password=samplepass sample' < php-sample-application-master/sql/db.sql
