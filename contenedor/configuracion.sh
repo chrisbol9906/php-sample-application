5a203a6ab0aa2685b30626e8333a6e24544a2882
chrisbol9906 <89878988+chrisbol9906@users.noreply.github.com>
2021-09-02 23:00:39 -0500

    Update configuracion.sh

--- contenedor/configuracion.sh
+++ contenedor/configuracion.sh
@@ -7,6 +7,7 @@ docker exec -i db_node sh -c 'exec mysql -u root --password=samplepass sample' <
 
 #conf app
 
+docker exec -i app_node apt install zip unzip -y
 docker exec -i app_node chown -R www-data:www-data /var/www/php-sample-application-master
 docker exec -i app_node cd && make
 docker exec -i app_node systemctl restart apache2
